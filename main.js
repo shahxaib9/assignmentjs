e4dbe9cf3a81b4cdd1df7c1c77eb79c91a4c4502
Abubakr <realchampiontheno1@gmail.com>
2020-12-27 16:20:18 +0500

    write main program in main.js

--- main.js
+++ main.js
@@ -1,3 +1,15 @@
 window.addEventListener('load', () => {
   console.log('Javascript Loaded!');
+
+  const tempCelsius = +prompt('Enter a temperature in celsius: ');
+  const tempFahrenheit = (9 / 5) * tempCelsius + 32;
+
+  const outputString =
+    'Temprature in Celsius: ' +
+    tempCelsius.toFixed(2) +
+    '°C \nTemprature in Fahrenheit: ' +
+    tempFahrenheit.toFixed(2) +
+    '°F';
+
+  alert(outputString);
 });
