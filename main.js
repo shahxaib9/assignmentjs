e87f42ee6410331d90ac9d7d41e782c4100643ae
Abubakr <realchampiontheno1@gmail.com>
2020-12-27 16:31:26 +0500

    write main program in main.js

--- main.js
+++ main.js
@@ -1,3 +1,11 @@
 window.addEventListener('load', () => {
   console.log('Javascript Loaded!');
+
+  const number = +prompt('Enter a number: ');
+
+  const isEven = number % 2 === 0;
+
+  const outputString = number + ' is ' + (isEven ? 'Even.' : 'Odd.');
+
+  alert(outputString);
 });
